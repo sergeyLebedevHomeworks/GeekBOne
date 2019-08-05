d81dc983b86c070e9c3371101d445168e2c169d2
sergeynine <s9@>
2019-08-05 11:56:26 +0300

    first home work

---
+++ inLink/inLink/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  inLink
+//
+//  Created by Sergey Lebedev on 03/08/2019.
+//  Copyright © 2019 Sergey Lebedev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWasShown(notification: Notification) {
+        let info = notification.userInfo! as NSDictionary
+        let size = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let insets = UIEdgeInsets(top: 0, left: 0, bottom: size.height, right: 0)
+        
+        self.scrollView.contentInset = insets
+        self.scrollView.scrollIndicatorInsets = insets
+    }
+    
+    @objc func keyboardWillHidden(notification: Notification) {
+        let insets = UIEdgeInsets.zero
+        self.scrollView.contentInset = insets
+        self.scrollView.scrollIndicatorInsets = insets
+    }
+    @IBAction func loginButtonTap(_ sender: Any) {
+        let login = self.loginTextField.text ?? "nan"
+        let pass = self.passwordTextField.text ?? "nan"
+        
+        if login == "user" && pass == "password" {
+            print("Успешно")
+        } else {
+            print("Ошибка")
+        }
+        
+    }
+}
+ 
